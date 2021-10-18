e372d47d310e08618daaa43a0f79c250397dfb92
Nikolay Radoev <nikolay.radoev@polymtl.ca>
2021-10-18 10:44:11 -0400

    Initial commit

---
+++ client/fetch_exemple.js
@@ -0,0 +1,34 @@
+const SERVER_URL = "http://localhost:5000";
+
+function obtenirCours(cours) {
+  const container = document.getElementById("cours-container");
+  container.innerHTML = ""; // on vide le conteneur
+  cours.forEach((c) => {
+    let cours_paragraph = document.createElement("p");
+    cours_paragraph.innerHTML = `<p>${c.sigle} : ${c.credits}</p>`;
+    container.appendChild(cours_paragraph);
+  });
+}
+
+function ajouterCours() {
+  const sigle = Math.floor(Math.random() * 6000 + 1000);
+  const credits = Math.floor(Math.random() * 5 + 1);
+  const cours = { sigle: `INF${sigle}`, credits: credits };
+
+  const opts = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cours),
+  };
+  const url = `${SERVER_URL}/ajouterCours`;
+  fetch(url, opts).then(() => init());
+}
+
+function init() {
+  const url = `${SERVER_URL}/obtenirCours`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((cours) => obtenirCours(cours));
+}
+
+window.onload = init;
---
+++ server/main.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const port = 5000;
+
+const app = express();
+app.use(cors());
+const urlencodedParser = express.urlencoded({ extended: false });
+app.use(express.json());
+
+const listeDeCours = [
+  { sigle: "LOG2990", credits: 4 },
+  { sigle: "LOG4420", credits: 3 },
+  { sigle: "LOG1000", credits: 3 },
+  { sigle: "MTH1102", credits: 2 },
+];
+
+
+app.get("/obtenirCours", function (req, res) {
+  res.send(listeDeCours);
+});
+
+app.get("/obtenirCours/:sigle", function (req, res) {
+  const cours = listeDeCours.find((c) => {
+    return c.sigle === req.params.sigle;
+  });
+  res.send(cours);
+});
+
+app.post("/ajouterCours", urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400);
+  const cours = { sigle: req.body.sigle, credits: parseInt(req.body.credits) };
+  listeDeCours.push(cours);
+  res.send(cours.sigle + " a été ajouté");
+});
+
+app.delete("/supprimerCours/:sigle", function (req, res) {
+  const taille = listeDeCours.length;
+  listeDeCours = listeDeCours.filter((c) => c.sigle !== req.params.sigle);
+  if (taille > listeDeCours.length) res.send("Cours supprimé.");
+  else res.send("Echec de suppression");
+});
+
+app.patch("/modifierCours/", urlencodedParser, function (req, res) {
+  const cours = listeDeCours.find((c) => {
+    return c.sigle === req.body.sigle;
+  });
+  if (!cours) return res.send("Ce cours n'existe pas");
+  cours.credits = req.body.credits;
+  res.send("Cours modifié");
+});
+
+app.listen(port);
